df8e50b7002d3a6af76526e4211ef2437e506a6c
cateru <caterina.baldassini@studio.unibo.it>
2026-09-22 16:12:51 +0200

    feat: Changed Gp, Gap, dG and Gin arrays based on the Riminucci, Langestein (2019) ArXiv model paper

--- approximation/approximation.py
+++ approximation/approximation.py
@@ -16,63 +16,40 @@ class TargetSolver:
         self.std_d2d = std_d2d
 
     def generate_noise(self):
-        self.pulses = np.arange(1, 15001)
-        self.d2d_a = self.rng.normal(loc=1, scale=self.std_d2d, size=self.N)
-        self.d2d_b = self.rng.normal(loc=0, scale=self.std_d2d, size=self.N)
-        self.d2d_c = self.rng.normal(loc=1, scale=self.std_d2d, size=self.N)
-        self.random_noise = self.rng.normal(loc=1, scale=self.std_noise, size=(self.N, len(self.pulses) + 1))
+        self.pulses = np.arange(1, 501)
+        self.d2d_Gth = self.rng.normal(loc=1, scale=self.std_d2d, size=self.N)
+        self.programming_noise = self.rng.normal(loc=1, scale=self.std_noise, size=(self.N, len(self.pulses) + 1))
         self.GP = np.array([[self.conductance_P(p, device=i) for p in self.pulses] for i in range(self.N)])
+        self.dG = np.array([[self.MG(p, device=i) for p in self.pulses] for i in range(self.N)])
         self.GAP = np.array([[self.conductance_AP(p, device=i) for p in self.pulses] for i in range(self.N)])
         self.Gin = np.array([[self.intermediate_conductance(p, device=i) for p in self.pulses] for i in range(self.N)])
-        self.dG = self.GAP - self.GP
-        self.GAP = np.array([[self.conductance_AP(p, same_noise=False, device=i) for p in self.pulses] for i in range(self.N)])
-        self.translate_conductance()
-        self.baseline = np.sum(self.Gin[:, 0] * self.d2d_c)
+        self.baseline = np.sum(self.Gin[:, 0])
 
+    def linear_func(self, pulse_number):
+        return 0.01 * pulse_number
 
-    def log_func(self, pulse_number, a = 2, b = 0):
-        return a * np.log10(pulse_number) + b
+    def conductance_P(self, pulse_number, device):
+        lin_func = self.linear_func(pulse_number)
+        return lin_func * self.programming_noise[device,pulse_number]
 
-    def conductance_P(self, pulse_number, device = 0, linear = False):
-        stretch = self.d2d_a[device]
-        shift = self.d2d_b[device]
-        if linear:            
-            line =  0.001 * pulse_number
-            return line * stretch * self.random_noise[device, pulse_number] + shift * 0.001
+    def MG(self, pulse_number, device):
+        d2d = self.d2d_Gth[device]
+        c = 0.05
+        Gth = 0.01
+        Gp = self.linear_func(pulse_number) 
+        if Gp < Gth * d2d:
+            dG = 0
         else:
-            log_func = self.log_func(pulse_number)
-            return log_func * stretch * self.random_noise[device,pulse_number] + shift * self.log_func(self.pulses[0])
+            dG = Gp * c * (Gp - (Gth * d2d)) ** (3/4)
+        return dG
 
-    def conductance_AP(self, pulse_number, same_noise = True, device = 0, linear = False):
-        stretch = self.d2d_a[device]
-        shift = self.d2d_b[device]
-        if linear: 
-            Gap = 0.0012 * pulse_number
-            return Gap * stretch * self.random_noise[device,pulse_number] + shift * 0.0012
-        else:
-            c = 3
-            x = pulse_number - c
-            Gap = self.log_func(x, a = 2.2)
-            if same_noise:
-                return (Gap + (self.random_noise[device,pulse_number]-1) * self.log_func(pulse_number)) * stretch + shift * self.log_func(self.pulses[0])
-            else:
-                return Gap * stretch * self.random_noise[device,pulse_number] + shift * self.log_func(self.pulses[0], a = 2.2)
+    def conductance_AP(self, pulse_number, device):
+        Gap = self.GP[device, pulse_number-1] + self.MG(pulse_number, device)
+        return Gap
 
     def intermediate_conductance(self, pulse_number, device = 0):
-        # log = self.log_func(pulse_number, a = 2.1)
-        # Gin = log * self.d2d_a[device] + self.d2d_b[device] * self.log_func(self.pulses[0], a = 2.1)
-        stretch = self.d2d_a[device]
-        shift = self.d2d_b[device]
-        Gin = 0.001 * pulse_number * stretch + shift * 0.001
+        Gin = self.linear_func(pulse_number) + (self.dG[device, pulse_number-1] / 2)
         return Gin 
-
-    def translate_conductance(self):
-        start_index = 14
-        self.pulses = self.pulses[start_index:]
-        self.GP = self.GP[:, start_index:]
-        self.GAP = self.GAP[:, start_index:]
-        self.dG = self.dG[:, start_index:]
-        self.Gin = self.Gin[:, start_index:]
 
     def closest_index(self, value, device=0):
         arr = self.dG[device]
@@ -137,12 +114,8 @@ class TargetSolver:
     def add_noise(self, sol, target):
         idx = sol["indices"]
         sign = np.sign(target)
-        if self.N < 2:
-            device, k = idx[0]
-            noisy_goal = self.Gin[device, k] * self.random_noise[device,k]
-        else: 
-            noisy_vals = [self.GP[device, k] * self.random_noise[device,k] for device, k in idx]
-            noisy_goal = sum(noisy_vals)
+        device, k = idx[0]
+        noisy_goal = self.Gin[device, k] * self.programming_noise[device,k]
         if sign > 0:
             noisy_total = noisy_goal - self.baseline
         else:
@@ -163,14 +136,12 @@ class TargetSolver:
         prev_error = float("inf")
         for base_idx in range(len(self.pulses)):
             sol = self.build_from_base(base_idx)
-            # logger.info(f"indexes={sol["indices"]} |" f"goal={sol["total"]}")
             total = sol["total"]
             if sign > 0:
                 total = total - self.baseline
             else:
                 total = -total + self.baseline
             rel_error = (abs(total - target) / abs(target))
-            # logger.info(f"total={total:.6f} | " f"rel_error={rel_error:.6f}")
             if rel_error > prev_error:
                 logger.info("Increasing error, stopping search")
                 break
@@ -182,61 +153,10 @@ class TargetSolver:
                     best["total"] = best["total"] - self.baseline
                 else:
                     best["total"] = -best["total"] + self.baseline         
-                # logger.info(f"NEW BEST | "f"base_idx={base_idx} | "f"error={best_error:.6f}")
         if best is not None:
             best["target_relative_error"] = best_error
-            # logger.info(f"Solution found | "f"error={best_error:.6f}")
         else: logger.warning("No valid solution found")
         logger.info(f"Best solution | target={best['total']:.6f} | error (%)={best['target_relative_error']:.6f} | indices={best['indices']}")
-        return best
-    
-                
-    def build_pair_sums(self):
-        A = np.asarray(self.GP)
-        n = len(A)
-        i_idx = np.repeat(np.arange(n), n)
-        j_idx = np.tile(np.arange(n), n)
-        sums = A[i_idx] + A[j_idx]
-        order = np.argsort(sums)
-        self.pair_values = sums[order]
-        self.pair_i = i_idx[order]
-        self.pair_j = j_idx[order]
-
-
-    def solve_no_MR(self, target):
-        sign = np.sign(target)
-        if sign < 0:
-            goal = self.baseline - target
-        else:
-            goal = self.baseline + target
-        A = np.asarray(self.GP)
-        n = len(A)
-        pair_values = self.pair_values
-        pair_i = self.pair_i
-        pair_j = self.pair_j
-        best_error = float("inf")
-        best = None
-        for k in range(n):
-            ak = A[k]
-            needed = goal - ak
-            idx = np.searchsorted(pair_values, needed)
-            for cand in (idx - 1, idx, idx + 1):
-                if 0 <= cand < len(pair_values):
-                    total = pair_values[cand] + ak
-                    i = pair_i[cand]
-                    j = pair_j[cand]
-                    if sign > 0:
-                        total_adj = total - self.baseline
-                    else:
-                        total_adj = -total + self.baseline
-                    rel_error = (abs(total_adj - target) / abs(target)) * 100
-                    if rel_error < best_error:
-                        best_error = rel_error
-                        best = {
-                            "indices": (i, j, k),
-                            "total": total_adj,
-                            "target_relative_error": best_error,
-                        }
         return best
     
     def plot_arrays(self, device=0):
